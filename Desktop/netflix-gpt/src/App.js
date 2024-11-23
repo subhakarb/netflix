b1ed5eefff8c3d9de0bb7a5ed61c71ece7d64c5c
subhakarb <subhakar.boyapati@gmail.com>
2024-11-23 10:40:45 +0530

    login changes

---
+++ Desktop/netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-gray-900'>Boyapati</h1>
+    </div>
+  );
+}
+
+export default App;
